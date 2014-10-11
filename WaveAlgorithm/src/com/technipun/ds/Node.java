42b07576180e7aa0deb2dc85a10e29588620028f
nipunbalan <nipunbalan@gmail.com>
2014-10-11 03:26:06 +0100

    Created Node class

--- WaveAlgorithm/src/com/technipun/ds/Node.java
+++ WaveAlgorithm/src/com/technipun/ds/Node.java
@@ -1,31 +1,89 @@
 package com.technipun.ds;
 
+/**
+ * @desc This class defines a Node
+ * @author Nipun Balan Thekkummal
+ * @version 1.0
+ */
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Node {
 
+	// Integer variable for unique nodeID
 	private int nodeID;
+
+	// Pointer to parent Node
+	private Node parent;
+
+	// Array List carrying the pointers to the child nodes
 	private ArrayList<Node> neigh;
 
-	public Node(int nodeID) {
-		this.setNodeID(nodeID);
-		this.setNeigh(new ArrayList<Node>());
+	// Constructor takes the nodeID and creates an empty ArrayList to store
+	// children.
+	public Node(int ID) {
+		nodeID = ID;
+		this.neigh = new ArrayList<Node>();
 	}
 
+	// Method to get the children (as an Array List of type Node)
+	public ArrayList<Node> getNeigh() {
+		if (this.neigh != null) {
+			return this.neigh;
+		} else {
+
+			return new ArrayList<Node>();
+		}
+	}
+
+	// Getter for nodeID
 	public int getNodeID() {
-		return nodeID;
+		return this.nodeID;
 	}
 
+	// Setter for nodeID
 	public void setNodeID(int nodeID) {
 		this.nodeID = nodeID;
 	}
 
-	public ArrayList<Node> getNeigh() {
-		return neigh;
+	//Getter for parent
+	public Node getParent() {
+		return this.parent;
 	}
 
-	public void setNeigh(ArrayList<Node> neigh) {
-		this.neigh = neigh;
+	//Method to add child node to a node
+	public void addNeigh(Node newNeigh) {
+		//Adding a new element to the ArrayList "children"
+		this.neigh.add(newNeigh);
+		newNeigh.setParent(this);
+	}
+
+	//Method to remove all child nodes.
+	public void removeAllNeighbours() {
+		//Iterating through the child list and removing the reference to its parents
+		Iterator<Node> neighItr = neigh.iterator();
+		while (neighItr.hasNext()) {
+			neighItr.next().setParent(null);
+		}
+		//Clearing the Arraylist "children" to remove all children"
+		this.neigh.clear();
+	}
+
+	//Method to remove a particular child referenced by nodeID
+	public void removeNeigh(int rmNodeID) {
+		//Iterator to check the nodeID in the children Array List and to delete the node if the ID matches
+		Iterator<Node> neighItr = neigh.iterator();
+		while (neighItr.hasNext()) {
+			Node currNode = neighItr.next();
+			if (currNode.nodeID == rmNodeID) {
+				currNode.setParent(null);
+				neighItr.remove();
+			}
+		}
+	}
+
+	private void setParent(Node parent) {
+		this.parent = parent;
 	}
 
 }
